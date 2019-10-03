273029c1c0a7dd19ca75933af2eb4f0de4f99ca1
Guilherme <guilherme-serrao.dev@outlook.pt>
2019-10-04 00:12:07 +0100

    init

---
+++ Assets/Scripts/FileManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FileManager : MonoBehaviour
+{
+    public Texture[,] grid; 
+
+    private string path;
+    private string[] files;
+
+    public int columns;
+    public int rows;
+
+    private int fileCounter = 0;
+
+    public RawImage image;
+
+    public Texture tempTexture;
+
+
+    public void Start()
+    {
+        grid = new Texture[columns, rows];
+    }
+
+    public void ChooseFolder()
+    {
+
+        path = EditorUtility.OpenFolderPanel("SelectFolder", "", "*.png");
+
+        files = Directory.GetFiles(path);
+
+        print(files.Length);
+
+        for (int i = 0; i < columns; i++)
+        {
+            for (int j = 0; j < rows; j++)
+            {
+                if (fileCounter < files.Length)
+                {
+
+                    WWW www = new WWW("file:///" + files[fileCounter]);
+                    tempTexture = www.texture;
+
+                    grid[i, j] = tempTexture;
+
+                    image.texture = grid[i, j];
+
+                    print("file" + fileCounter);
+                
+                    fileCounter++;
+                }                
+            }
+        }
+
+        if (fileCounter < files.Length)
+        {
+            print("Files missing");
+        }
+
+        SpawnGrid();
+    }
+
+    public void SpawnGrid()
+    {
+        for (int i = 0; i < columns; i++)
+        {
+            for (int j = 0; j < rows; j++)
+            {
+                //var image = Instantiate(new RawImage, transform.position, transform.rotation);
+            }
+        }
+    }
+
+}
+
